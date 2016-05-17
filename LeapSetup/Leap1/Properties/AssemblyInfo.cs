988bd0774d0a6616685c8786333644b592200297
Keiho Sakapon <saka_pon@live.jp>
2016-05-17 18:50:35 +0900

    Update AssemblyInfo

--- LeapSetup/Leap1/Properties/AssemblyInfo.cs
+++ LeapSetup/Leap1/Properties/AssemblyInfo.cs
@@ -1,18 +1,23 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Leap1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Monsoon.Setup.Leap1")]
+[assembly: AssemblyDescription("The library of basic setup for Leap Motion SDK 1.2.0.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Leap1")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("Monsoon")]
+[assembly: AssemblyCopyright("© 2016 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/sakapon/Monsoon.Setup")]
+[assembly: AssemblyMetadata("LicenseUrl", "https://github.com/sakapon/Monsoon.Setup/blob/master/LICENSE")]
+[assembly: AssemblyMetadata("Tags", "Leap Motion")]
+[assembly: AssemblyMetadata("ReleaseNotes", "The first release.")]
 
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
 // 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
@@ -34,3 +39,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: CLSCompliant(true)]
