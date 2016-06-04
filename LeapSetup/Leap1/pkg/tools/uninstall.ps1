0bc6e536cd97c702a808efbafabc9ad5df144198
Keiho Sakapon <saka_pon@live.jp>
2016-06-04 23:22:04 +0900

    Add uninstall.ps1

---
+++ LeapSetup/Leap1/pkg/tools/uninstall.ps1
@@ -0,0 +1,7 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$Leap_Item0 = $project.ProjectItems.Item("Leap.dll")
+$Leap_Item0.Delete()
+
+$LeapCSharp_Item0 = $project.ProjectItems.Item("LeapCSharp.dll")
+$LeapCSharp_Item0.Delete()
