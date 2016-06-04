8b36c74909c57a62cfb164cb26c3487ca315c416
Keiho Sakapon <saka_pon@live.jp>
2016-06-04 22:51:19 +0900

    Add project items as link

--- LeapSetup/Leap2/pkg/tools/install.ps1
+++ LeapSetup/Leap2/pkg/tools/install.ps1
@@ -1,9 +1,15 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$Leap_Item = $project.ProjectItems.Item("Leap.dll")
+$Leap_Item0 = $project.ProjectItems.Item("Leap.dll")
+$Leap_Item0.Delete()
+$Leap_Path = $installPath + "\content\Leap.dll"
+$Leap_Item = $project.ProjectItems.AddFromFile($Leap_Path)
 $Leap_OutputProp = $Leap_Item.Properties.Item("CopyToOutputDirectory")
 $Leap_OutputProp.Value = 2
 
-$LeapCSharp_Item = $project.ProjectItems.Item("LeapCSharp.dll")
+$LeapCSharp_Item0 = $project.ProjectItems.Item("LeapCSharp.dll")
+$LeapCSharp_Item0.Delete()
+$LeapCSharp_Path = $installPath + "\content\LeapCSharp.dll"
+$LeapCSharp_Item = $project.ProjectItems.AddFromFile($LeapCSharp_Path)
 $LeapCSharp_OutputProp = $LeapCSharp_Item.Properties.Item("CopyToOutputDirectory")
 $LeapCSharp_OutputProp.Value = 2
